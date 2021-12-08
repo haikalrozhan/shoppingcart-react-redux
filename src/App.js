6b1546112f1540ed52c0151d7ebcca11444821b9
haikalrozhan-tech <haikalrozhan1992@gmail.com>
2021-12-08 09:05:46 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        &copy;haikalrozhan 2022. All Rights Reserved.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
